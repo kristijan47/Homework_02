3891f38122bb4c14c76ff30e797c999dfa1536f7
kristijan47 <60441523+kristijan47@users.noreply.github.com>
2020-03-09 00:37:28 +0100

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -14,15 +14,15 @@ namespace Homework_02
             //Concatenate them in a new variable
             //Print the results in the console
             #region exercise1
-            //var stringOne = "Word ";
-            //var stringTwo = "Up";
-            //var result = stringOne + stringTwo;
+            var stringOne = "Word ";
+            var stringTwo = "Up";
+            var result = stringOne + stringTwo;
 
-            //Console.WriteLine($"Cameo: {result}");
-            //Console.ReadLine();
+            Console.WriteLine($"Cameo: {result}");
+            Console.ReadLine();
             #endregion
 
-            
+
             // concat strings to get result
             //var numOne = "9";
             //var numTwo = "3";
@@ -53,9 +53,50 @@ namespace Homework_02
             int numeroUno = 550;
             string someWords = " NZXT";
             var exerciseTwoResult = numeroUno + someWords;
+        
             Console.WriteLine(exerciseTwoResult);
             Console.ReadLine();
 
+            #endregion
+
+            #region exercise3
+            //            Exercise 03
+            //You have n credits on your mobile bill. One SMS costs m credits.How many SMS messages you can send?
+
+            //n = 102
+            //m = 5
+            //Result = ?
+
+            double n = 102;
+            double m = 5;
+            var  resultSmsCost = n / m;
+            Console.WriteLine($"One SMS costs {m} credits. You can send  {resultSmsCost} SMS messages");
+            Console.ReadLine();
+            #endregion
+
+            #region exercise4(bonus)
+            //    Exercise 04(*BONUS)
+            //Set variable from input
+            //TRY to convert it to int
+            //if its converted successfully print in the console if number is odd or even
+            //else if its not converted, print in the console that user inserted invalid input
+            //Hint: if number / 2 has remainder 0 its even, else its odd
+
+            Console.WriteLine("Just Type Something!");
+            var userInput = Console.ReadLine();
+            int inputNumber;
+            bool parsedNumber = int.TryParse(userInput, out inputNumber);
+            
+            if (parsedNumber) {
+                var evenOrOdd = inputNumber % 2 == 0 ? "even" : "odd";
+                Console.WriteLine(evenOrOdd );
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input. Please Try Again!");
+            }
+            Console.ReadLine();
+           
             #endregion
         }
     }
